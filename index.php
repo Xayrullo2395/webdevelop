efed5031a0d65c133876a40eb1f22243d53ccc14
Xayrulla <xayrullomatkarimov2288@gmail.com>
2019-10-28 09:15:24 +0300

    My First commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * Comment
+     */
+    $myVar = "First variable"; // string
+    // echo 'Salom dunyo';
+    $myInt = 25;
+    $myFloat = 25.5;
+    $myBool = true;
+    $myArray = [1,2,3];
+    // echo gettype($myArray);
+
+    echo 5/9;
+    echo '<h2>'.$myVar.'</h2>';
+?>
+
+<h3>TEst <?php echo $myVar ?></h3>
+
